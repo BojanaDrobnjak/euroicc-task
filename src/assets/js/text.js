dab15f637ab88a8217681ec4660fdfdccac7e1b5
unknown <bojanadrobnjak97@gmail.com>
2021-02-14 16:15:09 +0100

    creating table programmatically and retrieving xml data

--- src/assets/js/text.js
+++ src/assets/js/text.js
@@ -1,6 +1,6 @@
 export class Menu {
   insertText(json) {
-    const data = json.zadatak.menu;
+    const data = json.menu;
     Object.keys(data).forEach((key) => {
       const $elements = document.getElementsByClassName(`_js-menu--${key}`);
       Array.from($elements).forEach(($el) => {
@@ -12,7 +12,7 @@ export class Menu {
 
 export class Toggle {
   insertText(json) {
-    const data = json.zadatak.toggle;
+    const data = json.toggle;
     Object.keys(data).forEach((key) => {
       const $elements = document.getElementsByClassName(`_js-toggle--${key}`);
       Array.from($elements).forEach(($el) => {
@@ -24,7 +24,7 @@ export class Toggle {
 
 export class Heading {
   insertText(json) {
-    const data = json.zadatak.heading;
+    const data = json.heading;
     Object.keys(data).forEach((key) => {
       const $elements = document.getElementsByClassName(`_js-heading--${key}`);
       Array.from($elements).forEach(($el) => {
@@ -36,12 +36,46 @@ export class Heading {
 
 export class HeadlineText {
   insertText(json) {
-    const data = json.zadatak.headline_text;
+    const data = json.headline_text;
     Object.keys(data).forEach((key) => {
       const $elements = document.getElementsByClassName(`_js-headline_text--${key}`);
       Array.from($elements).forEach(($el) => {
         $el.innerHTML = data[key]._text;
       });
     });
+  }
+}
+
+export class Table {
+  insertTableHeadings(json) {
+    const data = json.columns.table_head;
+    Object.keys(data).forEach((key) => {
+      const $elements = document.getElementsByClassName(`_js-table_head--${key}`)
+      Array.from($elements).forEach(($el) => {
+        $el.innerHTML = data[key]._text;
+      })
+    })
+  }
+
+  insertTableTotal(json) {
+    const data = json.columns.table_total;
+    Object.keys(data).forEach((key) => {
+      const $elements = document.getElementsByClassName(`_js-table_total--${key}`)
+      Array.from($elements).forEach(($el) => {
+        $el.innerHTML = data[key]._text;
+      })
+    })
+  }
+
+  insertTableData(json) {
+    const data = json.columns.table_content.rows.row;
+    Object.keys(data).forEach((key) => {
+        Object.keys(data[key]).forEach((el)=> {
+          const $elements = document.getElementsByClassName(`_js-table_content--rows--row${key}--${el}`);
+          Array.from($elements).forEach(($el) => {
+            $el.innerHTML = data[key][el]._text;
+          })
+        })
+    })   
   }
 }
